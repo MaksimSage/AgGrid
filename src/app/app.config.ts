c5b09a9d7d8f07ec8c661fe49817a3059116a7a9
gibkiMM <gibkinms@gmail.com>
2026-07-19 16:29:31 +0300

    # 2: правки для теста деплоя

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,9 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideTaiga } from '@taiga-ui/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-    provideTaiga(),
     provideTaiga(),
   ],
 };

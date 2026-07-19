83c7a30688049ac6d34654fe1ea8e4cb55292a36
gibkiMM <gibkinms@gmail.com>
2026-07-19 16:14:15 +0300

    # 1: инит проекта

---
+++ src/app/app.config.ts
@@ -0,0 +1,14 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideTaiga } from '@taiga-ui/core';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideTaiga(),
+    provideTaiga(),
+  ],
+};
---
+++ src/app/app.ts
@@ -0,0 +1,92 @@
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from './core/header/header';
+import {
+  TuiAvatar,
+  TuiAvatarStack,
+  TuiBadge,
+  TuiChevron,
+  TuiChip,
+  TuiDataListWrapper,
+  TuiProgress,
+  TuiSelect,
+  TuiSkeleton,
+} from '@taiga-ui/kit';
+import {
+  TuiButton,
+  TuiCalendar,
+  TuiInput,
+  tuiItemsHandlersProvider,
+  TuiRoot,
+  TuiTextfield,
+} from '@taiga-ui/core';
+import { FormsModule } from '@angular/forms';
+import { TuiDay } from '@taiga-ui/cdk';
+import { TuiTable } from '@taiga-ui/addon-table';
+
+interface Character {
+  readonly id: number;
+  readonly name: string;
+  readonly site: string;
+}
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    RouterOutlet,
+    Header,
+    TuiRoot,
+    FormsModule,
+    TuiAvatar,
+    TuiButton,
+    TuiInput,
+    TuiSkeleton,
+    TuiTextfield,
+    TuiRoot,
+    TuiCalendar,
+    TuiDataListWrapper,
+    TuiSelect,
+    TuiChevron,
+    TuiTable,
+    TuiChip,
+    TuiBadge,
+    TuiProgress,
+    TuiAvatarStack,
+  ],
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    tuiItemsHandlersProvider({
+      stringify: signal((x: Character) => x.name),
+      identityMatcher: signal((a: Character, b: Character) => a.id === b.id),
+      disabledItemHandler: signal((x: Character) => x?.name?.includes('Trevor')),
+    }),
+  ],
+})
+export class App {
+  protected readonly title = signal('mood');
+
+  protected skeleton = true;
+  protected value: TuiDay | null = null;
+  protected readonly users: Character[] = [
+    { id: 1, name: 'Портал Госуслуги', site: 'https://www.gosuslugi.ru' },
+    { id: 2, name: 'ФНС России (Налоговая)', site: 'https://www.nalog.gov.ru' },
+    { id: 3, name: 'Социальный фонд России (СФР)', site: 'https://sfr.gov.ru' },
+    { id: 4, name: 'Росреестр', site: 'https://rosreestr.gov.ru' },
+    { id: 5, name: 'ГИБДД', site: 'https://гибдд.рф' },
+    { id: 6, name: 'Минцифры России', site: 'https://digital.gov.ru' },
+    { id: 7, name: 'Минздрав России', site: 'https://minzdrav.gov.ru' },
+    { id: 8, name: 'Центральный Банк РФ', site: 'https://www.cbr.ru' },
+    { id: 9, name: 'ФССП (Судебные приставы)', site: 'https://fssp.gov.ru' },
+    { id: 10, name: 'МВД России', site: 'https://мвд.рф' },
+    { id: 11, name: 'Роспотребнадзор', site: 'https://rospotrebnadzor.ru' },
+    { id: 12, name: 'Роструд', site: 'https://rostrud.gov.ru' },
+  ];
+
+  protected valueSelect: Character | null = this.users[0];
+
+  protected onDayClick(day: TuiDay): void {
+    this.value = day;
+  }
+}
